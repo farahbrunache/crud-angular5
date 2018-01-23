0a4d9731f59d6715bb3650d2c1d78813b9772b2b
manish <manis.kasyap@gmail.com>
2018-01-23 22:40:04 +0400

    opacity on hover

--- src/app/hover-effect.directive.ts
+++ src/app/hover-effect.directive.ts
@@ -1,10 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from "@angular/core";
 
 @Directive({
-  selector: '[appHoverEffect]'
+  selector: "[appHoverEffect]"
 })
 export class HoverEffectDirective {
+  constructor(private el: ElementRef) {}
 
-  constructor() { }
+  @HostListener("mouseenter")
+  onmouseenter() {
+    this.setOpacity(0.5);
+  }
 
+  @HostListener("mouseleave")
+  onmouseleave() {
+    this.setOpacity(1);
+  }
+
+  private setOpacity(opacity: number): void {
+    this.el.nativeElement.style.opacity = opacity;
+  }
 }
