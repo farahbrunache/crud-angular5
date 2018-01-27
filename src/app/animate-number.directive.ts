f13295a3205bf3210f887ba2fa93b4aee3c6d4a0
manish <manis.kasyap@gmail.com>
2018-01-27 18:06:54 +0400

    displaying pokemon count at top

--- src/app/animate-number.directive.ts
+++ src/app/animate-number.directive.ts
@@ -1,17 +1,19 @@
-import { Directive, ElementRef } from "@angular/core";
+import { Directive, ElementRef, Input } from "@angular/core";
 // import * as $ from "jquery";
 import { AfterViewInit } from "@angular/core/src/metadata/lifecycle_hooks";
+import { count } from "rxjs/operators/count";
 // import * as animateNumber from "jquery.animateNumber";
 
 @Directive({
   selector: "[appAnimateNumber]"
 })
 export class AnimateNumberDirective implements AfterViewInit {
+  @Input("appAnimateNumber") count: number;
   constructor(private el: ElementRef) {}
 
   ngAfterViewInit() {
     $(this.el.nativeElement).animateNumber({
-      number: 100
+      number: this.count
     });
     // $(this.el.nativeElement).css("color", "yellow");
   }
